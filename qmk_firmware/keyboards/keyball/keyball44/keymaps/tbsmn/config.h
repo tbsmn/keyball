513e84cf96932d3c6f993caee3b774d16dbfa73a
Tobias Simon <mail@tobiassimon.de>
2025-02-10 16:29:51 +0100

    added permissive hold

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 #define TAPPING_TERM 200
-
+#define PERMISSIVE_HOLD
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
