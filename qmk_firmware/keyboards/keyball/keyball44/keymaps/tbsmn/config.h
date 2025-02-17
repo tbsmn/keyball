91a8f63f49e442f2207b50b62d2bbab452eb19bd
Tobias Simon <mail@tobiassimon.de>
2025-02-17 21:17:04 +0100

    add quick tap term

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
@@ -35,6 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 #define TAPPING_TERM 200
+#define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
