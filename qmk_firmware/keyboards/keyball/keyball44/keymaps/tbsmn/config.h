7a46573f37b19601a1bd7e2c6caaf4e2984f78ea
Tobias Simon <mail@tobiassimon.de>
2025-03-28 10:56:34 +0100

    lower combo term

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
@@ -37,6 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
+#define COMBO_TERM 25
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
