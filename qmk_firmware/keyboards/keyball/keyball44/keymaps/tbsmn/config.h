9e5f7216eccd0cc73a01cb74efa0182c85cde43a
Tobias Simon <mail@tobiassimon.de>
2025-09-16 11:37:14 +0200

    change combo term to 25

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-#define COMBO_TERM 75
+#define COMBO_TERM 25
 
 // #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 // #define AUTO_MOUSE_DEFAULT_LAYER 1
