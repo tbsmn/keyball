7955ae747ba8a1514c79a11ce177158485ad9ebc
Tobias Simon <tobiassimon86@gmail.com>
2026-02-20 16:34:17 +0100

    enabled split detect OS

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
@@ -39,6 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define COMBO_TERM 25
+#define SPLIT_DETECTED_OS_ENABLE
 
 // #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 // #define AUTO_MOUSE_DEFAULT_LAYER 1
