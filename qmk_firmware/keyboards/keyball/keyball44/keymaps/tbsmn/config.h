6be0d443456d286a33e4cef6853e6883d7b02e80
Tobias Simon <tobiassimon86@gmail.com>
2026-03-24 17:09:04 +0100

    reset tapping term per key for scrolling

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM_PER_KEY
 #define QUICK_TAP_TERM 0
 #define PERMISSIVE_HOLD
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+// #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 #define COMBO_TERM 25
 #define SPLIT_DETECTED_OS_ENABLE
 // #define ACHORDION_STREAK
