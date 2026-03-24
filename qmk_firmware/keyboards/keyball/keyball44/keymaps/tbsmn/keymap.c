94a611e69431eefd05b3c9ebee45b3fcd6bfd3a7
Tobias Simon <tobiassimon86@gmail.com>
2026-03-24 08:41:06 +0100

    exlude non alpha keys from achordion

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
@@ -206,6 +206,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 void housekeeping_task_user(void) {
   achordion_task();
 }
+
+bool achordion_chord(uint16_t tap_hold_keycode,
+                     keyrecord_t* tap_hold_record,
+                     uint16_t other_keycode,
+                     keyrecord_t* other_record) {
+  // // Exceptionally consider the following chords as holds, even though they
+  // // are on the same hand in Dvorak.
+  // switch (tap_hold_keycode) {
+  //   case HOME_A:  // A + U.
+  //     if (other_keycode == HOME_U) { return true; }
+  //     break;
+  //
+  //   case HOME_S:  // S + H and S + G.
+  //     if (other_keycode == HOME_H || other_keycode == KC_G) { return true; }
+  //     break;
+  // }
+  //
+  // // Also allow same-hand holds when the other key is in the rows below the
+  // // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
+  // if (other_record->event.key.row % (MATRIX_ROWS / 2) >= 4) { return true; }
+
+  // Allow same-hand holds with non-alpha keys.
+  if (other_keycode > KC_Z) { return true; }
+
+  // Otherwise, follow the opposite hands rule.
+  return achordion_opposite_hands(tap_hold_record, other_record);
+}
+
 //-------
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
