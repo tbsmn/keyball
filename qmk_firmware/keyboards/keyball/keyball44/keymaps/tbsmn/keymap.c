f06d12d403067bf09bb8730b1a7d924f939890f3
Tobias Simon <tobiassimon86@gmail.com>
2026-03-24 15:06:44 +0100

    fix layer 2 tab mod key again

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
@@ -213,7 +213,9 @@ bool achordion_chord(uint16_t tap_hold_keycode,
                      keyrecord_t* other_record) {
   // // Exceptionally consider the following chords as holds, even though they
   // // are on the same hand in Dvorak.
-  // switch (tap_hold_keycode) {
+    switch (tap_hold_keycode) {
+      case LT(2, KC_TAB):
+        return true;
   //   case HOME_A:  // A + U.
   //     if (other_keycode == HOME_U) { return true; }
   //     break;
@@ -221,7 +223,7 @@ bool achordion_chord(uint16_t tap_hold_keycode,
   // case HOME_S:  // S + H and S + G.
   // if (other_keycode == HOME_H || other_keycode == KC_G) { return true; }
   // break;
-  // }
+  }
   //
   // // Also allow same-hand holds when the other key is in the rows below the
   // // alphas. I need the `% (MATRIX_ROWS / 2)` because my keyboard is split.
@@ -229,8 +231,6 @@ bool achordion_chord(uint16_t tap_hold_keycode,
 
   // Allow same-hand holds with non-alpha keys.
   if (other_keycode > KC_Z) { return true; }
-
-  if (other_keycode == LT(2, KC_TAB)) { return true; }
 
   // Otherwise, follow the opposite hands rule.
   return achordion_opposite_hands(tap_hold_record, other_record);
