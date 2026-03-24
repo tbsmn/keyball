5a4fc55e6739145a27e124bd40c44c3eb02938b2
Tobias Simon <tobiassimon86@gmail.com>
2026-03-24 14:50:56 +0100

    fix layer 2 tab mod key

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
@@ -218,9 +218,9 @@ bool achordion_chord(uint16_t tap_hold_keycode,
   //     if (other_keycode == HOME_U) { return true; }
   //     break;
   //
-  //   case HOME_S:  // S + H and S + G.
-  //     if (other_keycode == HOME_H || other_keycode == KC_G) { return true; }
-  //     break;
+  // case HOME_S:  // S + H and S + G.
+  // if (other_keycode == HOME_H || other_keycode == KC_G) { return true; }
+  // break;
   // }
   //
   // // Also allow same-hand holds when the other key is in the rows below the
@@ -229,6 +229,8 @@ bool achordion_chord(uint16_t tap_hold_keycode,
 
   // Allow same-hand holds with non-alpha keys.
   if (other_keycode > KC_Z) { return true; }
+
+  if (other_keycode == LT(2, KC_TAB)) { return true; }
 
   // Otherwise, follow the opposite hands rule.
   return achordion_opposite_hands(tap_hold_record, other_record);
