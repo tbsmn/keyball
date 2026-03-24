8344c2f3a7d0e1f2cf5f908b11673655678437e2
Tobias Simon <tobiassimon86@gmail.com>
2026-03-24 10:53:08 +0100

    try to fix eager mod

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tbsmn/keymap.c
@@ -234,8 +234,8 @@ bool achordion_chord(uint16_t tap_hold_keycode,
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
-bool achordion_eager_mod(uint8_t mod) {
-  switch (mod) {
+bool achordion_eager_mod(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
     case LT(3, KC_N):
       return true;
 
